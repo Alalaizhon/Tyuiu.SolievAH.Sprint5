b8e523d392e0498a689baae9905817a2bda6aa7e
Tyuiu.SolievAH@outlook.com <katagawa.k@gmail.com>
2023-11-18 21:00:34 +0500

    Add project files.

---
+++ Tyuiu.SolievAH.Sprint5.Task0.V4.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.SolievAH.Sprint5.Task0.V4.Lib
+{
+    public class DataService : ISprint5Task0V5
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
+
+            double F = (2* Math.Pow(x,3)) + (0.5 * Math.Pow(x, 2)) - (3.5 * x) + 2;
+
+            F = Math.Round(F, 3);
+
+            File.WriteAllText(path, Convert.ToString(F));
+            return path;
+
+        }
+    }
+}
---
+++ Tyuiu.SolievAH.Sprint5.Task0.V4.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Tyuiu.SolievAH.Sprint5.Task0.V4.Lib;
+
+namespace Tyuiu.SolievAH.Sprint5.Task0.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\katag\source\repos\Tyuiu.SolievAH.Sprint5\Tyuiu.SolievAH.Sprint5.Task0.V4\bin\Debug\OutOutFileTask0.txt";
+
+            FileInfo fileinfo = new FileInfo(path);
+
+            bool fileExists = fileinfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, true);
+        }
+    }
+}
