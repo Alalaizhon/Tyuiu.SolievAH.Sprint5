ecc2d93c57a0137cee9e93dae7dd56795e2b39f3
Tyuiu.SolievAH@outlook.com <katagawa.k@gmail.com>
2023-11-20 17:04:05 +0500

    Task2

---
+++ Tyuiu.SolievAH.Sprint5.Task2.V15.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SolievAH.Sprint5.Task2.V15.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SolievAH.Sprint5.Task2.V15.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b8a2c186-6f3d-4260-aa17-9d7ebd1354af")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
