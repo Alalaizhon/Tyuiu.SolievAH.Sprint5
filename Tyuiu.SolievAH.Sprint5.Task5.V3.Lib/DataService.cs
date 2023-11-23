d172b0702391abfb0a487ae6aa8f133025c70ae9
Tyuiu.SolievAH@outlook.com <katagawa.k@gmail.com>
2023-11-23 21:36:07 +0500

    Task5

---
+++ Tyuiu.SolievAH.Sprint5.Task5.V3.Lib/DataService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.SolievAH.Sprint5.Task5.V3.Lib
+{
+    public class DataService : ISprint5Task5V3
+    {
+        public double LoadFromDataFile(string path)
+        {
+            double res = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    double num = Convert.ToDouble(line);
+                    if (num % 1 != 0)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        res += num;
+                    }
+                }
+            }
+
+            return res; 
+        }
+    }
+}
---
+++ Tyuiu.SolievAH.Sprint5.Task5.V3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SolievAH.Sprint5.Task5.V3.Lib;
+
+
+namespace Tyuiu.SolievAH.Sprint5.Task5.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Солиев А.Х | СМАРТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый фай                         *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнил: Солиев Алижон Хотамжонович | СМАРТб-23-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            string path = @"C:\DataSprint5\InPutDataFileTask5V3.txt";
+            Console.WriteLine("Данные находятся в файле:" + path);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
