07c12764dc05a5b71d88607f6e1105e8d413b8fd
Tyuiu.SolievAH@outlook.com <katagawa.k@gmail.com>
2023-11-26 19:43:33 +0500

    Task7

---
+++ Tyuiu.SolievAH.Sprint5.Task7.V18.Lib/DataService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Tyuiu.SolievAH.Sprint5.Task7.V18.Lib
+{
+    public class DataService : ISprint5Task7V18
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string pathSaveFile = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask7.txt";
+            FileInfo fileInfo = new FileInfo(pathSaveFile);
+            bool fileExists = fileInfo.Exists;
+
+            if (fileExists)
+            {
+                File.Delete(pathSaveFile);
+            }
+
+            path = @"C:\DataSprint5\InPutDataFileTask7V18.txt";
+            string fileContent = File.ReadAllText(path);
+            string replace = Regex.Replace(fileContent, @"н", "нн");
+            File.AppendAllText(pathSaveFile, replace + Environment.NewLine);
+
+            return pathSaveFile ;
+
+            // https://pstrtns.screenshotmaker.repl.co/ 
+        }
+    }
+}
---
+++ Tyuiu.SolievAH.Sprint5.Task7.V18/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Tyuiu.SolievAH.Sprint5.Task7.V18.Lib;
+
+namespace Tyuiu.SolievAH.Sprint5.Task7.V18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил: Солиев А.Х | СМАРТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый фай                         *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнил: Солиев Алижон Хотамжонович | СМАРТб-23-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            string path = @"C:\DataSprint5\InPutDataFileTask5V3.txt";
+            string pathSaveFile = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask7.txt";
+            Console.WriteLine("Данные находятся в файле:" + path);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            pathSaveFile = ds.LoadDataAndSave(path);
+            Console.WriteLine("Файл находится по адресу:" + pathSaveFile);
+            Console.ReadKey();
+        }
+    }
+}
